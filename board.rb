b8db6b89923a1bb680e3f187527134f272d99b97
Gryyph <kevin.o.griffith@gmail.com>
2019-06-27 17:36:07 -0700

    add [], []=, render functions to board

--- board.rb
+++ board.rb
@@ -1,4 +1,4 @@
-require "tile.rb"
+require_relative "tile"
 
 
 class Board
@@ -17,10 +17,24 @@ class Board
 
     end
 
+    def [](pos)
+        x, y = pos
+        grid[x][y]
+    end
+
+    def []=(pos, value)
+        x, y = pos
+        tile = grid[x][y]
+        tile.value = value
+    end
 
 
-
-
+    def render
+        puts "  #{(0..8).to_a.join(" ")}"
+        grid.each_with_index do |row, i|
+            puts "#{i} #{row.join(" ")}"
+        end
+    end
 
 
 
