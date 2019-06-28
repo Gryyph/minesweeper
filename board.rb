d0fb214fdda95981783140594b35ae7685c1b5c4
Gryyph <kevin.o.griffith@gmail.com>
2019-06-28 14:30:42 -0700

    add bomb generation and rendering

--- board.rb
+++ board.rb
@@ -1,5 +1,5 @@
 require_relative "tile"
-
+require "byebug"
 
 class Board
 
@@ -12,16 +12,9 @@ class Board
 
     end
 
-    def generate_bombs
-        
-
-
-    end
-
-
     def initialize(grid = self.empty_grid)
         @grid = grid
-        @grid.generate_bombs
+        self.generate_bombs
 
     end
 
@@ -30,11 +23,10 @@ class Board
         grid[x][y]
     end
 
-    def []=(pos, value)
-        x, y = pos
-        tile = grid[x][y]
-        tile.value = value
-    end
+    #def []=(pos, value)
+        #x, y = pos
+        #tile = grid[x][y]
+        ##end
 
 
     def render
@@ -49,13 +41,47 @@ class Board
         end
     end
 
+    def render_bombs
+        puts "  #{(0..8).to_a.join(" ")}"
+        grid.each_with_index do |row, i|
+            arr = []
+            row.each do |tile|
+                if tile.contains_bomb == true
+                    arr << "B"
+                else
+                    arr<< tile.display_value
+                end
+            end
+
+            puts "#{i} #{arr.join(" ")}"
+        end
+    end
 
 
+    def generate_bombs
+        total_bomb_count = rand(1...81)
+        total_bomb_count.times do 
+            set_bomb
+        end
+    end
 
-
-
-
-
-
+    def set_bomb
+        row = rand(0..8)
+        col = rand(0..8)
+        pos = [row,col]
+        #debugger
+        if self[pos].contains_bomb == true
+            until self[pos].contains_bomb == false
+                row = rand(0..8)
+                col = rand(0..8)
+                pos = [row,col]
+            end
+            self[pos].contains_bomb = true
+        else
+            self[pos].contains_bomb = true
+        end
+        
+        
+    end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,6 +1,6 @@
 class Tile
 
-    #attr_accessor :value
+    attr_accessor :display_value, :contains_bomb, :flagged, :revealed, :neighboring_bomb_count
 
     def initialize()
         @display_value = "*"
