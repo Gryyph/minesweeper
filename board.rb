020ecd3912933967441b440048232d33c6f6fe80
Gryyph <kevin.o.griffith@gmail.com>
2019-07-02 15:17:06 -0700

    complete board generation

--- board.rb
+++ board.rb
@@ -15,7 +15,8 @@ class Board
     def initialize(grid = self.empty_grid)
         @grid = grid
         self.generate_bombs
-        self.generate_adacent_tiles
+        self.generate_adjacent_tiles
+        #self.render
 
     end
 
@@ -35,7 +36,13 @@ class Board
         grid.each_with_index do |row, i|
             arr = []
             row.each do |tile|
-                arr << tile.display_value
+                if tile.revealed == true
+                    arr << tile.display_value
+                elsif tile.flagged == true
+                    arr << "F"
+                else
+                    arr << "*"
+                end
             end
 
             puts "#{i} #{arr.join(" ")}"
@@ -60,7 +67,7 @@ class Board
 
 
     def generate_bombs
-        total_bomb_count = rand(1...81)
+        total_bomb_count = rand(1..20)
         total_bomb_count.times do 
             set_bomb
         end
@@ -85,7 +92,59 @@ class Board
     end
 
     def generate_adjacent_tiles
-       
+       @grid.each_with_index do |row,i|
+            row.each_with_index do |tile, j|
+                bomb_count = count_helper(i,j)
+                #puts "#{i} and #{j} were successful!"
+                if bomb_count == 0
+                        tile.display_value = "_"
+                elsif bomb_count.nil?
+                        tile.display_value = "B"
+                else
+                        tile.display_value = bomb_count.to_s
+                end
+                
+            end
+        end
     end
 
+
+    def count_helper(row_index, col_index)
+        pos = [row_index, col_index]
+        count = 0
+        if self[pos].contains_bomb == true
+            return nil
+        elsif row_index < 8 #any row between first and last
+            start_row = row_index - 1
+            start_col = col_index - 1
+            (start_row..(row_index+1)).each do |i|
+                (start_col..(col_index + 1)).each do |j|
+                    if self[[i,j]].nil?
+                        next
+                    elsif self[[i,j]].contains_bomb == true
+                        count += 1
+                    else
+                        next
+                    end
+                end
+            end
+
+        else #last row
+            start_row = row_index - 1
+            start_col = col_index - 1
+            (start_row..(row_index)).each do |i|
+                (start_col..(col_index + 1)).each do |j|
+                    if self[[i,j]].nil?
+                        next
+                    elsif self[[i,j]].contains_bomb == true
+                        count += 1
+                    else
+                        next
+                    end
+                end
+            end
+        end
+
+        return count
+    end
 end
--- tile.rb
+++ tile.rb
@@ -7,7 +7,6 @@ class Tile
         @contains_bomb = false
         @flagged = false
         @revealed = false
-        @neighboring_bomb_count = nil
     end
 
 
